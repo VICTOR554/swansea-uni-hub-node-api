9d1789cd16cbe36d3fb21a6150c01b9faf7bb8b6
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-08-28 19:14:19 +0100

    added dotenv, npm, nodemon to the project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+//Load config vars
+dotenv.config({ path: './config/config.env'});
+
+const app = express();
+
+const PORT = process.env.PORT || 6000;
+
+app.listen(PORT, 
+    console.log(`Server runnin in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
