40f989e91ac0f1fbb0f761ebc06629f2fac46624
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-08-30 20:29:53 +0100

    Created CRUD(create, read, update and delete) handlers for student, as well as their routes.

---
+++ apps/admin/admin.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+
+const student = require('./student/student');
+
+router.use('/student', student);
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const admin = require('./apps/admin/admin')
 
 //Load config vars
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
-
+//Mount admin routers
+app.use('/admin', admin)
 
 const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, 
-    console.log(`Server runnin in ${process.env.NODE_ENV} mode on port ${PORT}`)
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
