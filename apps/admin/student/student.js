7807ea16d0e160d1193030af87bfc059721dbf8a
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-08-30 20:08:22 +0100

    Update CRUD handler

--- apps/admin/student/student.js
+++ apps/admin/student/student.js
@@ -6,7 +6,7 @@ const handlers = require('./student.handlers');
 router.get('/', handlers.getAllStudents)
 router.get('/:id', handlers.getOneStudent)
 router.post('/new', handlers.createStudents)
-router.put('/edit/:number', handlers.updateStudent)
-
+router.put('/update/:id', handlers.updateStudent)
+router.delete('/delete/:id', handlers.deleteStudent)
 
 module.exports = router;
