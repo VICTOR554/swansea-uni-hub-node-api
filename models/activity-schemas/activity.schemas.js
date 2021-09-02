b1d7c557838c1bb02dbbbd14bc0f0f2d85c2b6e6
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-09-02 02:05:19 +0100

    Created activity schema

---
+++ models/activity-schemas/activity.schemas.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const activitySchema = new Schema({
+    moduleCode: {
+        type: String,
+        required: true
+    },
+    lecturerName: {
+        type: String,
+        required: true
+    },
+    startTime: {
+        type: Number,
+        required: true
+    },
+    endTime:{
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['Lecture', 'Seminar', 'Lab', 'Exam', 'Lecture/Seminar', 'Revision'],
+        required: true
+    },
+})
+
+
+module.exports = activitySchema
--- models/model.js
+++ models/model.js
@@ -2,10 +2,14 @@ const Student =  require('./student-schemas/student.schema');
 const Module =  require('./module-schemas/module.schema');
 const Note =  require('./note-schemas/note.schema');
 const Task = require('./task-schemas/task.schema');
+const Activity = require('./activity-schemas/activity.schema');
+
 
 module.exports = {
     Student,
     Module,
     Note,
-    Task
+    Task,
+    Activity
+
 }
