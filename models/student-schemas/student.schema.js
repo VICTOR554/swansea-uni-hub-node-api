5d215e4f760538c3bb92943771659992ba1badfe
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-09-03 23:28:19 +0100

    updated student schema

--- models/student-schemas/student.schema.js
+++ models/student-schemas/student.schema.js
@@ -22,6 +22,34 @@ const studentSchema = new Schema({
         type: Number,
         required: true
     },
+    degree: {
+        type: String,
+        enum: ['Foundation', 'A-level', 'First year', 'Second year', 'Third year', 'Master', 'Phd'],
+        required: true
+    },
+    finance: {
+        tuition: {
+            initial: {
+                type: String,
+                required: true
+            },
+            payment: [{
+                type: Number,
+            }],
+        },
+        accomodation: {
+            initial: {
+                type: String,
+            },
+            payment: [{
+                type: Number,
+            }],
+        },
+    },
+    address: {
+        type: String,
+        required: true
+    },
     specialism: {
         type: String,
     },
