ae2d2b7a7cf5c19c951876e7f6e8637f45ac7ae8
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-04 22:20:25 +0100

    updated student.js in student route

--- apps/student/student.js
+++ apps/student/student.js
@@ -4,6 +4,5 @@ const router = express.Router()
 const handlers = require('./student.handlers');
 
 
-router.get('/', handlers.getAllStudents)
 router.get('/:id', handlers.getStudent)
 router.put('/update/:id', handlers.updateStudent)
