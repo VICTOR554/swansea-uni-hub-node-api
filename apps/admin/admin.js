0f225d75631809d86c9f4283296fd0a7a573745d
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-04 22:37:26 +0100

    Updated admin.js with all the routes in admin folder

--- apps/admin/admin.js
+++ apps/admin/admin.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express.Router()
 
-
+// Routes
 const student = require('./student/student');
+const activity = require('./activity/activity');
+const course = require('./course/course');
+const lecturer = require('./lecturer/lecturer');
+const location = require('./location/location');
+const modules = require('./module/module');
+const report = require('./report/report');
+const session = require('./session/session');
+const term = require('./term/term');
+const week = require('./week/week');
 
 
-
-
+// Mounting the Routes and how it is displayed in the url
 router.use('/students', student);
+router.use('/activities', activity);
+router.use('/courses', course);
+router.use('/lecturers', lecturer);
+router.use('/locations', location);
+router.use('/modules', modules);
+router.use('/reports', report);
+router.use('/sessions', session);
+router.use('/terms', term);
+router.use('/weeks', week);
+
+
 
 module.exports = router;
