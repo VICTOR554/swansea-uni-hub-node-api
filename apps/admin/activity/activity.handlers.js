41f40c1c781b9e8198f07e9a878bbbe9c4e97f02
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-13 18:01:15 +0100

    Updated CRUD function in activity handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/activity/activity.handlers.js
+++ apps/admin/activity/activity.handlers.js
@@ -1,39 +1,100 @@
+const model = require('../../../models/model');
 
 //@des      get all activities
 //@route    GET /activities
 //@access   Admin
-const getAllActivities = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all activities`});
-  };
+const getAllActivities = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.find();
+
+    res.status(200).json({
+      success: true,
+      count: activity.length,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      get one activities
 //@route    GET /activities/:id
 //@access   Admin
-const getOneActivity = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get activity ${req.params.id}`});
-  };
+const getOneActivity = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.findById(req.params.id);
+
+    if (!activity) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Create activity
 //@route    POST /activities/new
 //@access   Admin
-const createActivities = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new activities`});
-  };
+const createActivities = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Update activity
 //@route    PUT /activities/update/id
 //@access   Admin
-const updateActivity = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update activity ${req.params.id}`});
-  };
+const updateActivity = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!activity) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Delete activity
 //@route    DELETE /activities/delete/id
 //@access   Admin
-const deleteActivity = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete activity ${req.params.id}`});
-};
+const deleteActivity = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.findByIdAndDelete(req.params.id);
 
+    if (!activity) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 module.exports = {
   getAllActivities,
@@ -41,5 +102,4 @@ module.exports = {
   createActivities,
   updateActivity,
   deleteActivity
- 
-}
+};
