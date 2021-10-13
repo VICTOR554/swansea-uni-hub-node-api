cd7064df745bad40441c8bf7ffeb82b4e1d3f71a
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-13 22:34:54 +0100

    Updated CRUD function in course handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/course/course.handlers.js
+++ apps/admin/course/course.handlers.js
@@ -1,39 +1,100 @@
+const model = require('../../../models/model');
 
 //@des      get all courses
 //@route    GET /courses
 //@access   Admin
-const getAllCourses = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all courses`});
-  };
+const getAllCourses = async (req, res, next) => {
+  try {
+    const course = await model.Course.find();
+
+    res.status(200).json({
+      success: true,
+      count: course.length,
+      data: course
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      get one course
 //@route    GET /courses/:id
 //@access   Admin
-const getOneCourse = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get course ${req.params.id}`});
-  };
+const getOneCourse = async (req, res, next) => {
+  try {
+    const course = await model.Course.findById(req.params.id);
+
+    if (!course) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: course
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Create courses
 //@route    POST /courses/new
 //@access   Admin
-const createCourses = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new courses`});
-  };
+const createCourses = async (req, res, next) => {
+  try {
+    const course = await model.Course.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: course
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Update course
 //@route    PUT /courses/update/id
 //@access   Admin
-const updateCourse = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update course ${req.params.id}`});
-  };
+const updateCourse = async (req, res, next) => {
+  try {
+    const course = await model.Course.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!course) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: course
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Delete course
 //@route    DELETE /courses/delete/id
 //@access   Admin
-const deleteCourse = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete course ${req.params.id}`});
-};
+const deleteCourse = async (req, res, next) => {
+  try {
+    const course = await model.Activity.findByIdAndDelete(req.params.id);
 
+    if (!course) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 module.exports = {
   getAllCourses,
@@ -41,5 +102,4 @@ module.exports = {
   createCourses,
   updateCourse,
   deleteCourse
- 
-}
+};
