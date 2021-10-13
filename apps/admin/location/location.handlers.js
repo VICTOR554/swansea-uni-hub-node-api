dd1e4132627301d2a093484f5f059e6b48e9f159
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-13 23:02:10 +0100

    Updated location function in lecturer handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/location/location.handlers.js
+++ apps/admin/location/location.handlers.js
@@ -1,45 +1,105 @@
+const model = require('../../../models/model');
 
 //@des      get all locations
 //@route    GET /locations
 //@access   Admin
-const getAllLocations = (req, res, next) => { 
-  res.status(200).json({ success: true, msg: `Show all locations`});
+const getAllLocations = async (req, res, next) => {
+  try {
+    const location = await model.Location.find();
+
+    res.status(200).json({
+      success: true,
+      count: location.length,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des      get one location
 //@route    GET /locations/:id
 //@access   Admin
-const getOneLocation = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get location ${req.params.id}`});
+const getOneLocation = async (req, res, next) => {
+  try {
+    const location = await model.Location.findById(req.params.id);
+
+    if (!location) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des      Create locations
 //@route    POST /locations/new
 //@access   Admin
-const createLocations = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Create new locations`});
+const createLocations = async (req, res, next) => {
+  try {
+    const location = await model.Location.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des      Update location
 //@route    PUT /locations/update/id
 //@access   Admin
-const updateLocation = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Update location ${req.params.id}`});
+const updateLocation = async (req, res, next) => {
+  try {
+    const location = await model.Location.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!location) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des      Delete location
 //@route    DELETE /locations/delete/id
 //@access   Admin
-const deleteLocation = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Delete location ${req.params.id}`});
+const deleteLocation = async (req, res, next) => {
+  try {
+    const location = await model.Location.findByIdAndDelete(req.params.id);
+
+    if (!location) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
-
 module.exports = {
-getAllLocations,
-getOneLocation,
-createLocations,
-updateLocation,
-deleteLocation
-
-}
+  getAllLocations,
+  getOneLocation,
+  createLocations,
+  updateLocation,
+  deleteLocation
+};
