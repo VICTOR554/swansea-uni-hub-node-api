c1488e20297b3fc8b106bdc344223ea655131c14
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-13 23:18:40 +0100

    Updated CRUD function in module handler to send data to the database. Also, added try and catch to show any error
    CRUD- create, read, update and delete

--- apps/admin/module/module.handlers.js
+++ apps/admin/module/module.handlers.js
@@ -1,38 +1,100 @@
+const model = require('../../../models/model');
+
 //@des      get all modules
 //@route    GET /modules
 //@access   Admin
-const getAllModules = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all modules`});
-  };
+const getAllModules = async (req, res, next) => {
+  try {
+    const module = await model.Module.find();
 
-//@des      get one students
+    res.status(200).json({
+      success: true,
+      count: module.length,
+      data: module
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
+
+//@des      get one module
 //@route    GET /modules/:id
 //@access   Admin
-const getOneModule = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get modules ${req.params.id}`});
-  };
+const getOneModule = async (req, res, next) => {
+  try {
+    const module = await model.Module.findById(req.params.id);
+
+    if (!module) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: module
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Create modules
 //@route    POST /modules/new
 //@access   Admin
-const createModules = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new modules`});
-  };
+const createModules = async (req, res, next) => {
+  try {
+    const module = await model.Module.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: module
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Update module
 //@route    PUT /modules/update/id
 //@access   Admin
-const updateModule = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update modules ${req.params.id}`});
-  };
+const updateModule = async (req, res, next) => {
+  try {
+    const module = await model.Module.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!module) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: module
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Delete module
 //@route    DELETE /modules/delete/id
 //@access   Admin
-const deleteModule = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete modules ${req.params.id}`});
-};
+const deleteModule = async (req, res, next) => {
+  try {
+    const module = await model.Module.findByIdAndDelete(req.params.id);
 
+    if (!module) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 module.exports = {
   getAllModules,
@@ -40,5 +102,4 @@ module.exports = {
   createModules,
   updateModule,
   deleteModule
- 
-}
+};
