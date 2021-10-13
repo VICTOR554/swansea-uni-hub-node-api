5bfb98bca30156843600be1d3c7b49106fa68a02
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-13 23:49:07 +0100

    Updated CRUD function in report handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/Report/report.handlers.js
+++ apps/admin/Report/report.handlers.js
@@ -1,38 +1,95 @@
+const model = require('../../../models/model');
 
 //@des      get all reports
 //@route    GET /reports
 //@access   Admin
-const getAllReports = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all reports`});
-  };
+const getAllReports = async (req, res, next) => { 
+  try {
+    const report = await model.Report.find();
+
+    res.status(200).json({
+      success: true,
+      count: report.length,
+      data: report
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      get one report
 //@route    GET /reports/:id
 //@access   Admin
-const getOneReport = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get report ${req.params.id}`});
-  };
+const getOneReport = async (req, res, next) => {
+  try {
+    const report = await model.Report.findById(req.params.id);
+
+    if (!report) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: report
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Create reports
 //@route    POST /reports/new
 //@access   Admin
-const createReports = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new reports`});
-  };
+const createReports = async (req, res, next) => {
+  try {
+    const report = await model.Report.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: report
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Update report
 //@route    PUT /reports/update/id
 //@access   Admin
-const updateReport = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update report ${req.params.id}`});
-  };
+const updateReport = async (req, res, next) => {
+  try {
+    const report = await model.Report.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!report) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: report
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Delete report
 //@route    DELETE /reports/delete/id
 //@access   Admin
-const deleteReport = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete report ${req.params.id}`});
-};
+const deleteReport = async (req, res, next) => {
+  try {
+    const report = await model.Report.findByIdAndDelete(req.params.id);
+
+    if (!report) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }};
 
 
 module.exports = {
