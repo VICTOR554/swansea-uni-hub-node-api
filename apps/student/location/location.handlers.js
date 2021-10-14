04c9fc018efbb1c5ea682d863e48a34ffcf8a752
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 07:58:41 +0100

    Updated CRUD function in location handler at student route to send data to the database. Also, added try and catch to view any error

--- apps/student/location/location.handlers.js
+++ apps/student/location/location.handlers.js
@@ -1,23 +1,44 @@
+const model = require('../../../models/model');
 
 //@des      get all locations
 //@route    GET /locations
-//@access   Student
-const getAllLocations = (req, res, next) => { 
-  res.status(200).json({ success: true, msg: `Show all locations`});
+//@access   Admin
+const getAllLocations = async (req, res, next) => {
+  try {
+    const location = await model.Location.find();
+
+    res.status(200).json({
+      success: true,
+      count: location.length,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 //@des      get one location
 //@route    GET /locations/:id
-//@access   Student
-const getOneLocation = (req, res, next) => {
-  res.status(200).json({ success: true, msg: `Get location ${req.params.id}`});
+//@access   Admin
+const getOneLocation = async (req, res, next) => {
+  try {
+    const location = await model.Location.findById(req.params.id);
+
+    if (!location) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: location
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
 };
 
 
-
 module.exports = {
-getAllLocations,
-getOneLocation,
-
-
-}
+  getAllLocations,
+  getOneLocation
+};
