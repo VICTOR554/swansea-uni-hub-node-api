b0437539cda3b12723ef2583fdc62988089bb4ef
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 08:00:14 +0100

    Updated CRUD function in week handler at student route to send data to the database. Also, added try and catch to view any error

--- apps/student/week/week.handlers.js
+++ apps/student/week/week.handlers.js
@@ -1,21 +1,44 @@
+const model = require('../../../models/model');
 
 //@des      get all weeks
 //@route    GET /weeks
-//@access   Student
-const getAllWeeks = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all weeks`});
-  };
+//@access   Admin
+const getAllWeeks = async (req, res, next) => { 
+  try {
+    const week = await model.Week.find();
+
+    res.status(200).json({
+      success: true,
+      count: week.length,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      get one week
 //@route    GET /weeks/:id
-//@access   Student
-const getOneWeek = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get week ${req.params.id}`});
-  };
+//@access   Admin
+const getOneWeek = async (req, res, next) => {
+  try {
+    const week = await model.Week.findById(req.params.id);
+
+    if (!week) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
+
+
 
 module.exports = {
   getAllWeeks,
-  getOneWeek,
-
+  getOneWeek
  
 }
