e2ff0b7eaf0f5d9b3fa7d1e6b8a6f41bbd608441
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 07:20:43 +0100

    Updated CRUD function in note handler to send data to the database. Also, added try and catch to show any error

--- apps/student/Note/note.handlers.js
+++ apps/student/Note/note.handlers.js
@@ -1,38 +1,95 @@
+const model = require('../../../models/model');
 
 //@des      get all notes
 //@route    GET /notes
 //@access   Student
-const getAllNotes = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all notes`});
-  };
+const getAllNotes = async (req, res, next) => { 
+  try {
+    const note = await model.Note.find();
+
+    res.status(200).json({
+      success: true,
+      count: note.length,
+      data: note
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      get one note
 //@route    GET /notes/:id
 //@access   Student
-const getOneNote = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get note ${req.params.id}`});
-  };
+const getOneNote = async (req, res, next) => {
+  try {
+    const note = await model.Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: note
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Create note
 //@route    POST /notes/new
 //@access   Student
-const createNotes = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new notes`});
-  };
+const createNotes = async (req, res, next) => {
+  try {
+    const note = await model.Note.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: note
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Update note
 //@route    PUT /notes/update/id
 //@access   Student
-const updateNote = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update note ${req.params.id}`});
-  };
+const updateNote = async (req, res, next) => {
+  try {
+    const note = await model.Note.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!note) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: note
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Delete note
 //@route    DELETE /notes/delete/id
 //@access   Student
-const deleteNote = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete note ${req.params.id}`});
-};
+const deleteNote = async (req, res, next) => {
+  try {
+    const note = await model.Note.findByIdAndDelete(req.params.id);
+
+    if (!note) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }};
 
 
 module.exports = {
