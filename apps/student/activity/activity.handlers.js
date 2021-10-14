24e5240f378e3d5a527b5e8da2d500ac1d49192a
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 07:57:35 +0100

    Updated CRUD function in activity handler at student route to send data to the database. Also, added try and catch to view any error

--- apps/student/activity/activity.handlers.js
+++ apps/student/activity/activity.handlers.js
@@ -1,24 +1,45 @@
+const model = require('../../../models/model');
 
 //@des      get all activities
 //@route    GET /activities
-//@access   Student
-const getAllActivities = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all activities`});
-  };
+//@access   Admin
+const getAllActivities = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.find();
+
+    res.status(200).json({
+      success: true,
+      count: activity.length,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      get one activities
 //@route    GET /activities/:id
-//@access   Student
-const getOneActivity = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get activity ${req.params.id}`});
-  };
+//@access   Admin
+const getOneActivity = async (req, res, next) => {
+  try {
+    const activity = await model.Activity.findById(req.params.id);
 
+    if (!activity) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: activity
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 
 
 module.exports = {
   getAllActivities,
-  getOneActivity,
-  
- 
-}
+  getOneActivity
+};
