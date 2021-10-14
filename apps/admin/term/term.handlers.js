241a103b5a49042c346655dc1225bf393007450d
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 06:59:18 +0100

    Updated CRUD function in term handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/term/term.handlers.js
+++ apps/admin/term/term.handlers.js
@@ -1,38 +1,95 @@
+const model = require('../../../models/model');
 
 //@des      get all terms
 //@route    GET /terms
 //@access   Admin
-const getAllTerms = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all terms`});
-  };
+const getAllTerms = async (req, res, next) => { 
+  try {
+    const term = await model.Term.find();
+
+    res.status(200).json({
+      success: true,
+      count: term.length,
+      data: term
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      get one term
 //@route    GET /terms/:id
 //@access   Admin
-const getOneTerm = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get term ${req.params.id}`});
-  };
+const getOneTerm = async (req, res, next) => {
+  try {
+    const term = await model.Term.findById(req.params.id);
+
+    if (!term) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: term
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Create terms
 //@route    POST /terms/new
 //@access   Admin
-const createTerms = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new terms`});
-  };
+const createTerms = async (req, res, next) => {
+  try {
+    const term = await model.Term.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: term
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Update term
 //@route    PUT /terms/update/id
 //@access   Admin
-const updateTerm = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update term ${req.params.id}`});
-  };
+const updateTerm = async (req, res, next) => {
+  try {
+    const term = await model.Term.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!term) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: term
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Delete term
 //@route    DELETE /terms/delete/id
 //@access   Admin
-const deleteTerm = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete term ${req.params.id}`});
-};
+const deleteTerm = async (req, res, next) => {
+  try {
+    const term = await model.Term.findByIdAndDelete(req.params.id);
+
+    if (!term) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }};
 
 
 module.exports = {
