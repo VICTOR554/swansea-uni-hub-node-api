82c5750580025dbd52aa4f1ffe83bde13aaa86be
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 07:18:19 +0100

    Updated CRUD function in week handler to send data to the database. Also, added try and catch to show any error

--- apps/admin/week/week.handlers.js
+++ apps/admin/week/week.handlers.js
@@ -1,38 +1,95 @@
+const model = require('../../../models/model');
 
 //@des      get all weeks
 //@route    GET /weeks
 //@access   Admin
-const getAllWeeks = (req, res, next) => { 
-    res.status(200).json({ success: true, msg: `Show all weeks`});
-  };
+const getAllWeeks = async (req, res, next) => { 
+  try {
+    const week = await model.Week.find();
+
+    res.status(200).json({
+      success: true,
+      count: week.length,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      get one week
 //@route    GET /weeks/:id
 //@access   Admin
-const getOneWeek = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get week ${req.params.id}`});
-  };
+const getOneWeek = async (req, res, next) => {
+  try {
+    const week = await model.Week.findById(req.params.id);
+
+    if (!week) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Create weeks
 //@route    POST /weeks/new
 //@access   Admin
-const createWeeks = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Create new weeks`});
-  };
+const createWeeks = async (req, res, next) => {
+  try {
+    const week = await model.Week.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Update week
 //@route    PUT /weeks/update/id
 //@access   Admin
-const updateWeek = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update week ${req.params.id}`});
-  };
+const updateWeek = async (req, res, next) => {
+  try {
+    const week = await model.Week.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
+
+    if (!week) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: week
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }  };
 
 //@des      Delete week
 //@route    DELETE /weeks/delete/id
 //@access   Admin
-const deleteWeek = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Delete week ${req.params.id}`});
-};
+const deleteWeek = async (req, res, next) => {
+  try {
+    const week = await model.Week.findByIdAndDelete(req.params.id);
+
+    if (!week) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: {}
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }};
 
 
 module.exports = {
