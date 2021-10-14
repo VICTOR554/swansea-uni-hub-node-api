b454488861e725b29dc1a35116820ba0ad8c37e5
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-14 07:30:51 +0100

    Updated CRUD function in student handler at student route to send data to the database. Also, added try and catch to view any error

--- apps/student/student.handlers.js
+++ apps/student/student.handlers.js
@@ -1,19 +1,50 @@
+const model = require('../../../models/model');
+
 //@des      get one student
-//@route    GET /:id
-//@access   Student
-const getStudent = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Get student ${req.params.id}`});
-  };
+//@route    GET /students/:id
+//@access   Admin
+const getOneStudent = async (req, res, next) => {
+  try {
+    const student = await model.Student.findById(req.params.id);
+
+    if (!student) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: student
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
+  }
+};
 
 //@des      Update student
-//@route    PUT /update/id
-//@access   Student
-const updateStudent = (req, res, next) => {
-    res.status(200).json({ success: true, msg: `Update student ${req.params.id}`});
-  };
+//@route    PUT /students/update/id
+//@access   Admin
+const updateStudent = async (req, res, next) => {
+  try {
+    const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
 
-  module.exports = {
-    getStudent,
-    updateStudent,
-   
+    if (!student) {
+      return res.status(400).json({ success: false });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: student
+    });
+  } catch (err) {
+    res.status(400).json({ success: false });
   }
+};
+
+
+module.exports = {
+  getOneStudent,
+  updateStudent,
+};
