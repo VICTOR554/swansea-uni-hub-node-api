5841ece50b9452c795140ca8a30a15b3288b44c8
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-10-20 14:25:58 +0100

    Update student.js

--- apps/student/student.js
+++ apps/student/student.js
@@ -3,6 +3,26 @@ const router = express.Router()
 
 const handlers = require('./student.handlers');
 
-
 router.get('/:id', handlers.getStudent)
 router.put('/update/:id', handlers.updateStudent)
+
+// Routes
+const activity = require('./activity/activity');
+const location = require('./location/location');
+const modules = require('./module/module');
+const week = require('./week/week');
+const coursework = require('./coursework/coursework');
+const finance = require('./finance/finance');
+
+
+// Mounting the Routes and how it is displayed in the url
+router.use('/activities', activity);
+router.use('/locations', location);
+router.use('/modules', modules);
+router.use('/weeks', week);
+router.use('/courseworks', coursework);
+router.use('/finances', finance);
+
+
+
+module.exports = router;
