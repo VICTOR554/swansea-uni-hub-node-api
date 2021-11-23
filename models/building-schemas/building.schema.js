626cc0646ece95bdfe001663a025288214c01dcf
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 15:03:48 +0000

    Created building schema

---
+++ models/building-schemas/building.schema.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+});
+
+module.exports = bookingSchema;
