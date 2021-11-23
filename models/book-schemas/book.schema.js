826d6f14508b77650e8fa196bff96db08e680925
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 15:32:26 +0000

    created book schema

---
+++ models/book-schemas/book.schema.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  subject: {
+    type: String,
+    required: true
+  },
+  publisher: {
+    type: String,
+    required: true
+  },
+  created_at: {
+    type: String,
+    required: true
+  },
+  availability: {
+    type: String,
+    enum: ['Bay', 'Singleton', 'Bay and Singleton', 'Not available'],
+    required: true
+  },
+  bookmarked: {
+    type: Boolean,
+    required: true
+  }
+});
+
+module.exports = bookSchema;
