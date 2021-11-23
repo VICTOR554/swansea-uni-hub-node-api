ecac6016944e6cbc0c20f43ca833b1e2fc0ded84
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 18:13:47 +0000

    updated task schema

--- models/task-schemas/task.schema.js
+++ models/task-schemas/task.schema.js
@@ -1,40 +1,40 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
-    studentNumber:{
-        type: Number,
-        required: true
-    },
-    moduleCode: {
-        type: String,
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    dueDate: {
-        type: Number,
-        required: true
-    },
-    initialDate: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        //required: true
-    },
-    flagged: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-    completed: {
-        type: Boolean,
-        required: true,
-        default: false
-    },
-})
+  student_number: {
+    type: Number,
+    required: true
+  },
+  module_code: {
+    type: String
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  due_date_time: {
+    type: Number,
+    required: true
+  },
+  created_date_time: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String
+    //required: true
+  },
+  flagged: {
+    type: Boolean,
+    required: true,
+    default: false
+  },
+  completed: {
+    type: Boolean,
+    required: true,
+    default: false
+  }
+});
 
-module.exports = taskSchema
+module.exports = taskSchema;
