5bfce75bfe1e35fcfb641fe08232ac9b3f6da7c9
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 17:53:19 +0000

    updated note and report schema format and data name

--- models/note-schemas/note.schema.js
+++ models/note-schemas/note.schema.js
@@ -1,27 +1,26 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const noteSchema = new Schema({
-    studentNumber: {
-        type: Number,
-        required: true
-    },
-    moduleCode: {
-        type: String,
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    initialdate: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
+  student_number: {
+    type: Number,
+    required: true
+  },
+  module_code: {
+    type: String
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  created_date_time: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
 
-})
-
-module.exports = noteSchema
+module.exports = noteSchema;
--- models/report-schemas/report.schema.js
+++ models/report-schemas/report.schema.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const reportSchema = new Schema({
-    studentNumber: {
-        type: Number,
-        required: true
-    },
-    title: {
-        type: String,
-        required: true
-    },
-    topic: {
-        type: String,
-        required: true
-    },
-    initialdate: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
+  student_number: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  },
+  topic: {
+    type: String,
+    required: true
+  },
+  created_date_time: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  }
+});
 
-})
-
-module.exports = reportSchema
+module.exports = reportSchema;
