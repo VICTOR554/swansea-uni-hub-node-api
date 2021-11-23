b0ae1eba9bf4612c31d6392b93f42140e46842d3
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 15:24:59 +0000

    Created room schema

---
+++ models/room-schemas/room.schema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const roomSchema = new Schema({
+    number: {
+        type: String,
+        required: true
+    },
+    building_name: {
+        type: Number,
+        required: true
+    },
+})
+
+
+module.exports = roomSchema;
