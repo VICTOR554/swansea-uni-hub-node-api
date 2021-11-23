12194498a6f461f52bc29243cecfc6e5f0f70dfd
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-23 18:09:29 +0000

    updated term and week schema

--- models/term-schemas/term.schema.js
+++ models/term-schemas/term.schema.js
@@ -1,23 +1,21 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const termSchema = new Schema({
-    number: {
-        type: Number,
-        enum: [1,2,3],
-        required: true,
-        unique: true
-    },
-    startWeek: {
-        type: Number,
-        required: true
-    },
-    endWeek: {
-        type: Number,
-        required: true
-    },
-})
+  number: {
+    type: Number,
+    enum: [1, 2, 3],
+    required: true,
+    unique: true
+  },
+  startWeek: {
+    type: Number,
+    required: true
+  },
+  endWeek: {
+    type: Number,
+    required: true
+  }
+});
 
-
-
-module.exports = termSchema
+module.exports = termSchema;
--- models/week-schemas/week.schema.js
+++ models/week-schemas/week.schema.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const weekSchema = new Schema({
-    number: {
-        type: Number,
-        unique:true
-    },
-    dates: [{
-        type: Number,
-    }],
-})
+  number: {
+    type: Number,
+    unique: true,
+    required: true
+  },
+  dates: [
+    {
+      type: Number,
+      required: true
+    }
+  ]
+});
 
-
-module.exports = weekSchema
+module.exports = weekSchema;
