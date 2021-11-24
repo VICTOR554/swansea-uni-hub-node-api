b3e008c46ae59f9abdfa03b5c08c82c9f4ccf1a0
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-24 20:17:19 +0000

    Updated error middleware to have duplicate key response

--- middleware/error.js
+++ middleware/error.js
@@ -1,18 +1,25 @@
-const ErrorResponse = require("../utils/errorResponse");
+const ErrorResponse = require('../utils/errorResponse');
 
 const errorHandler = (err, req, res, next) => {
-    let error = {...err}
+  let error = { ...err };
 
-    error.message = err.message;
-  // Log to console 
+  error.message = err.message;
+  // Log to console
   console.log(err.stack.red);
 
-   // Bad objectId
-   if (err.name === 'CastError') {
-    const message = `Student not found with id of ${err.value}`; 
-    error = new ErrorResponse(message, 404)
-   }
+  // Bad objectId
+  if (err.name === 'CastError') {
+    const message = `Student not found with id of ${err.value}`;
+    error = new ErrorResponse(message, 404);
+  }
 
+  // Duplicate key
+  if (err.code === 11000) {
+    const message = 'Duplicate field value entered';
+    error = new ErrorResponse(message, 400);
+  }
+
+  
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
