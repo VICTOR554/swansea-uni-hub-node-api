de26175d72e6905ffa269d453ae66162e0153ad1
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-24 20:36:01 +0000

    Updated error middleware to have validation error response

--- middleware/error.js
+++ middleware/error.js
@@ -19,7 +19,12 @@ const errorHandler = (err, req, res, next) => {
     error = new ErrorResponse(message, 400);
   }
 
-  
+  // Validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message);
+    error = new ErrorResponse(message, 400);
+  }
+
   res.status(error.statusCode || 500).json({
     success: false,
     error: error.message || 'Server Error'
