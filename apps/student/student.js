7984d3f003da1fed27a279c7b98808f8df2a0979
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-24 15:47:31 +0000

    Updated student.js with the new routes

--- apps/student/student.js
+++ apps/student/student.js
@@ -8,22 +8,44 @@ router.put('/update/:id', handlers.updateStudent);
 
 // Routes
 const activity = require('./activity/activity');
-const location = require('./location/location');
-const modules = require('./module/module');
-const week = require('./week/week');
+const book = require('./book/book');
+const booking = require('./booking/booking');
+const building = require('./building/building');
+const campus = require('./campus/campus');
+const course = require('./course/course');
 const coursework = require('./coursework/coursework');
+const event = require('./event/event');
 const finance = require('./finance/finance');
 const lecturer = require('./lecturer/lecturer');
+const location = require('./location/location');
+const modules = require('./module/module');
+const note = require('./note/note');
 const report = require('./report/report');
+const room = require('./room/room');
+const society = require('./society/society');
+const sport = require('./sport/sport');
+const task = require('./task/task');
+const week = require('./week/week');
 
 // Mounting the Routes and how it is displayed in the url
 router.use('/activities', activity);
-router.use('/locations', location);
-router.use('/modules', modules);
-router.use('/weeks', week);
+router.use('/books', book);
+router.use('/bookings', booking);
+router.use('/buildings', building);
+router.use('/campuses', campus);
+router.use('/courses', course);
 router.use('/courseworks', coursework);
+router.use('/events', event);
 router.use('/finances', finance);
 router.use('/lecturers', lecturer);
+router.use('/locations', location);
+router.use('/modules', modules);
+router.use('/notes', note);
 router.use('/reports', report);
+router.use('/rooms', room);
+router.use('/societies', society);
+router.use('/sports', sport);
+router.use('/tasks', task);
+router.use('/weeks', week);
 
 module.exports = router;
