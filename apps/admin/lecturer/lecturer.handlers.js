deec62ba67615bec020d42a40ba81eaa794aa56d
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:00:52 +0000

    Updated lecturer handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/lecturer/lecturer.handlers.js
+++ apps/admin/lecturer/lecturer.handlers.js
@@ -1,96 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all lecturer
 //@route    GET /lecturers
 //@access   Admin
-const getAllLecturers = async (req, res, next) => { 
-  try {
-    const lecturer = await model.Lecturer.find();
+const getAllLecturers = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.find();
 
-    res.status(200).json({
-      success: true,
-      count: lecturer.length,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: lecturer.length,
+    data: lecturer
+  });
+});
 
 //@des      get one lecturer
 //@route    GET /lecturers/:id
 //@access   Admin
-const getOneLecturer = async (req, res, next) => {
-  try {
-    const lecturer = await model.Lecturer.findById(req.params.id);
+const getOneLecturer = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.findById(req.params.id);
 
-    if (!lecturer) {
-      return res.status(400).json({ success: false });
-    }
+  if (!lecturer) {
+    return res.status(400).json({ success: false });
+  }
 
-    res.status(200).json({
-      success: true,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: lecturer
+  });
+});
 
 //@des      Create lecturers
 //@route    POST /lecturers/new
 //@access   Admin
-const createLecturers = async (req, res, next) => {
-  try {
-    const lecturer = await model.Lecturer.create(req.body);
+const createLecturers = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(201).json({
+    success: true,
+    data: lecturer
+  });
+});
 
 //@des      Update lecturer
 //@route    PUT /lecturers/update/id
 //@access   Admin
-const updateLecturer = async (req, res, next) => {
-  try {
-    const lecturer = await model.Lecturer.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateLecturer = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!lecturer) {
-      return res.status(400).json({ success: false });
-    }
+  if (!lecturer) {
+    return res.status(400).json({ success: false });
+  }
 
-    res.status(200).json({
-      success: true,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: lecturer
+  });
+});
 
 //@des      Delete lecturer
 //@route    DELETE /lecturers/delete/id
 //@access   Admin
-const deleteLecturer = async (req, res, next) => {
-  try {
-    const lecturer = await model.Lecturer.findByIdAndDelete(req.params.id);
+const deleteLecturer = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.findByIdAndDelete(req.params.id);
 
-    if (!lecturer) {
-      return res.status(400).json({ success: false });
-    }
+  if (!lecturer) {
+    return res.status(400).json({ success: false });
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }};
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllLecturers,
@@ -98,5 +84,4 @@ module.exports = {
   createLecturers,
   updateLecturer,
   deleteLecturer
- 
-}
+};
--- apps/student/lecturer/lecturer.handlers.js
+++ apps/student/lecturer/lecturer.handlers.js
@@ -1,44 +1,37 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all lecturer
 //@route    GET /lecturers
 //@access   Student
-const getAllLecturers = async (req, res, next) => { 
-  try {
-    const lecturer = await model.Lecturer.find();
+const getAllLecturers = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.find();
 
-    res.status(200).json({
-      success: true,
-      count: lecturer.length,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: lecturer.length,
+    data: lecturer
+  });
+});
 
 //@des      get one lecturer
 //@route    GET /lecturers/:id
 //@access   Student
-const getOneLecturer = async (req, res, next) => {
-  try {
-    const lecturer = await model.Lecturer.findById(req.params.id);
+const getOneLecturer = asyncHandler(async (req, res, next) => {
+  const lecturer = await model.Lecturer.findById(req.params.id);
 
-    if (!lecturer) {
-      return res.status(400).json({ success: false });
-    }
+  if (!lecturer) {
+    return res.status(400).json({ success: false });
+  }
 
-    res.status(200).json({
-      success: true,
-      data: lecturer
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
-
-
+  res.status(200).json({
+    success: true,
+    data: lecturer
+  });
+});
 
 module.exports = {
   getAllLecturers,
-  getOneLecturer,
- 
-}
+  getOneLecturer
+};
