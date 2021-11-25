c8e4fe3d8b61dee8c992a559a06dc966a3c45d5e
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 10:46:20 +0000

    Updated campus handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/campus/campus.handlers.js
+++ apps/admin/campus/campus.handlers.js
@@ -1,100 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all campuses
 //@route    GET /campuses
-//@access   Admin 
-const getAllCampuses = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.find();
+//@access   Admin
+const getAllCampuses = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.find();
 
-    res.status(200).json({
-      success: true,
-      count: campus.length,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: campus.length,
+    data: campus
+  });
+});
 
 //@des      get one campus
 //@route    GET /campuses/:id
 //@access   Admin
-const getOneCampus = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.findById(req.params.id);
+const getOneCampus = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.findById(req.params.id);
 
-    if (!campus) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!campus) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: campus
+  });
+});
 
 //@des      Create campuses
 //@route    POST /campuses/new
 //@access   Admin
-const createCampuses= async (req, res, next) => {
-  try {
-    const campus = await model.Campus.create(req.body);
+const createCampuses = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: campus
+  });
+});
 
 //@des      Update campus
 //@route    PUT /campuses/update/id
 //@access   Admin
-const updateCampus = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateCampus = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!campus) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!campus) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: campus
+  });
+});
 
 //@des      Delete campus
 //@route    DELETE /campuses/delete/id
 //@access   Admin
-const deleteCampus = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.findByIdAndDelete(req.params.id);
+const deleteCampus = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.findByIdAndDelete(req.params.id);
 
-    if (!campus) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!campus) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllCampuses,
--- apps/student/campus/campus.handlers.js
+++ apps/student/campus/campus.handlers.js
@@ -1,41 +1,35 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all campuses
 //@route    GET /campuses
 //@access   Student 
-const getAllCampuses = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.find();
+const getAllCampuses = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.find();
 
-    res.status(200).json({
-      success: true,
-      count: campus.length,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: campus.length,
+    data: campus
+  });
+});
 
 //@des      get one campus
 //@route    GET /campuses/:id
 //@access   Student
-const getOneCampus = async (req, res, next) => {
-  try {
-    const campus = await model.Campus.findById(req.params.id);
+const getOneCampus = asyncHandler(async (req, res, next) => {
+  const campus = await model.Campus.findById(req.params.id);
 
-    if (!campus) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: campus
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!campus) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: campus
+  });
+});
 
 
 module.exports = {
