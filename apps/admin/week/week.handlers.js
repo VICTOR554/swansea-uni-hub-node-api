61ec95dfff8a622afcdb413c185229849c274e6f
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:27:27 +0000

    Updated week handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/week/week.handlers.js
+++ apps/admin/week/week.handlers.js
@@ -1,96 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all weeks
 //@route    GET /weeks
 //@access   Admin
-const getAllWeeks = async (req, res, next) => { 
-  try {
-    const week = await model.Week.find();
+const getAllWeeks = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.find();
 
-    res.status(200).json({
-      success: true,
-      count: week.length,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: week.length,
+    data: week
+  });
+});
 
 //@des      get one week
 //@route    GET /weeks/:id
 //@access   Admin
-const getOneWeek = async (req, res, next) => {
-  try {
-    const week = await model.Week.findById(req.params.id);
+const getOneWeek = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.findById(req.params.id);
 
-    if (!week) {
-      return res.status(400).json({ success: false });
-    }
+  if (!week) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: week
+  });
+});
 
 //@des      Create weeks
 //@route    POST /weeks/new
 //@access   Admin
-const createWeeks = async (req, res, next) => {
-  try {
-    const week = await model.Week.create(req.body);
+const createWeeks = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(201).json({
+    success: true,
+    data: week
+  });
+});
 
 //@des      Update week
 //@route    PUT /weeks/update/id
 //@access   Admin
-const updateWeek = async (req, res, next) => {
-  try {
-    const week = await model.Week.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateWeek = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!week) {
-      return res.status(400).json({ success: false });
-    }
+  if (!week) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: week
+  });
+});
 
 //@des      Delete week
 //@route    DELETE /weeks/delete/id
 //@access   Admin
-const deleteWeek = async (req, res, next) => {
-  try {
-    const week = await model.Week.findByIdAndDelete(req.params.id);
+const deleteWeek = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.findByIdAndDelete(req.params.id);
 
-    if (!week) {
-      return res.status(400).json({ success: false });
-    }
+  if (!week) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }};
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllWeeks,
@@ -98,5 +84,4 @@ module.exports = {
   createWeeks,
   updateWeek,
   deleteWeek
- 
-}
+};
--- apps/student/week/week.handlers.js
+++ apps/student/week/week.handlers.js
@@ -1,44 +1,37 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all weeks
 //@route    GET /weeks
 //@access   Student
-const getAllWeeks = async (req, res, next) => { 
-  try {
-    const week = await model.Week.find();
+const getAllWeeks = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.find();
 
-    res.status(200).json({
-      success: true,
-      count: week.length,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: week.length,
+    data: week
+  });
+});
 
 //@des      get one week
 //@route    GET /weeks/:id
 //@access   Student
-const getOneWeek = async (req, res, next) => {
-  try {
-    const week = await model.Week.findById(req.params.id);
+const getOneWeek = asyncHandler(async (req, res, next) => {
+  const week = await model.Week.findById(req.params.id);
 
-    if (!week) {
-      return res.status(400).json({ success: false });
-    }
+  if (!week) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: week
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
-
-
+  res.status(200).json({
+    success: true,
+    data: week
+  });
+});
 
 module.exports = {
   getAllWeeks,
   getOneWeek
- 
-}
+};
