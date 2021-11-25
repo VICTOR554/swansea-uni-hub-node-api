4903d4c0f4970f20c87c14201f0d75a8b0cde179
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:24:20 +0000

    Updated sport handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/sport/sport.handlers.js
+++ apps/admin/sport/sport.handlers.js
@@ -1,100 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all sports
 //@route    GET /sports
-//@access   Admin 
-const getAllSports = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.find();
+//@access   Admin
+const getAllSports = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.find();
 
-    res.status(200).json({
-      success: true,
-      count: sport.length,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: sport.length,
+    data: sport
+  });
+});
 
 //@des      get one sport
 //@route    GET /sports/:id
 //@access   Admin
-const getOneSport = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.findById(req.params.id);
+const getOneSport = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.findById(req.params.id);
 
-    if (!sport) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!sport) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: sport
+  });
+});
 
 //@des      Create sports
 //@route    POST /sports/new
 //@access   Admin
-const createSports = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.create(req.body);
+const createSports = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: sport
+  });
+});
 
 //@des      Update sport
 //@route    PUT /sports/update/id
 //@access   Admin
-const updateSport = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateSport = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!sport) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!sport) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: sport
+  });
+});
 
 //@des      Delete sport
 //@route    DELETE /sports/delete/id
 //@access   Admin
-const deleteSport = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.findByIdAndDelete(req.params.id);
+const deleteSport = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.findByIdAndDelete(req.params.id);
 
-    if (!sport) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!sport) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllSports,
--- apps/student/sport/sport.handlers.js
+++ apps/student/sport/sport.handlers.js
@@ -1,41 +1,36 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all sports
 //@route    GET /sports
-//@access   Student 
-const getAllSports = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.find();
+//@access   Student
+const getAllSports = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.find();
 
-    res.status(200).json({
-      success: true,
-      count: sport.length,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: sport.length,
+    data: sport
+  });
+});
 
 //@des      get one sport
 //@route    GET /sports/:id
 //@access   Student
-const getOneSport = async (req, res, next) => {
-  try {
-    const sport = await model.Sport.findById(req.params.id);
+const getOneSport = asyncHandler(async (req, res, next) => {
+  const sport = await model.Sport.findById(req.params.id);
 
-    if (!sport) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: sport
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!sport) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: sport
+  });
+});
+
 module.exports = {
   getAllSports,
   getOneSport
