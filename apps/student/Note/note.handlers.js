6c949bc60e976a53d94563927afea3a5153ea382
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:56:11 +0000

    Updated note handler in student route to use error response, error handler and async handler.

--- apps/student/Note/note.handlers.js
+++ apps/student/Note/note.handlers.js
@@ -1,96 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all notes
 //@route    GET /notes
 //@access   Student
-const getAllNotes = async (req, res, next) => { 
-  try {
-    const note = await model.Note.find();
+const getAllNotes = asyncHandler(async (req, res, next) => {
+  const note = await model.Note.find();
 
-    res.status(200).json({
-      success: true,
-      count: note.length,
-      data: note
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: note.length,
+    data: note
+  });
+});
 
 //@des      get one note
 //@route    GET /notes/:id
 //@access   Student
-const getOneNote = async (req, res, next) => {
-  try {
-    const note = await model.Note.findById(req.params.id);
+const getOneNote = asyncHandler(async (req, res, next) => {
+  const note = await model.Note.findById(req.params.id);
 
-    if (!note) {
-      return res.status(400).json({ success: false });
-    }
+  if (!note) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: note
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: note
+  });
+});
 
 //@des      Create note
 //@route    POST /notes/new
 //@access   Student
-const createNotes = async (req, res, next) => {
-  try {
-    const note = await model.Note.create(req.body);
+const createNotes = asyncHandler(async (req, res, next) => {
+  const note = await model.Note.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: note
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(201).json({
+    success: true,
+    data: note
+  });
+});
 
 //@des      Update note
 //@route    PUT /notes/update/id
 //@access   Student
-const updateNote = async (req, res, next) => {
-  try {
-    const note = await model.Note.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateNote = asyncHandler(async (req, res, next) => {
+  const note = await model.Note.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!note) {
-      return res.status(400).json({ success: false });
-    }
+  if (!note) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: note
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: note
+  });
+});
 
 //@des      Delete note
 //@route    DELETE /notes/delete/id
 //@access   Student
-const deleteNote = async (req, res, next) => {
-  try {
-    const note = await model.Note.findByIdAndDelete(req.params.id);
+const deleteNote = asyncHandler(async (req, res, next) => {
+  const note = await model.Note.findByIdAndDelete(req.params.id);
 
-    if (!note) {
-      return res.status(400).json({ success: false });
-    }
+  if (!note) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }};
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllNotes,
@@ -98,5 +84,4 @@ module.exports = {
   createNotes,
   updateNote,
   deleteNote
- 
-}
+};
