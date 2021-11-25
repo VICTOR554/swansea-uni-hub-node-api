59fc871cff172fd8f108fb8011fbcbc1696da77e
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:15:27 +0000

    Updated report handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/Report/report.handlers.js
+++ apps/admin/Report/report.handlers.js
@@ -1,96 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all reports
 //@route    GET /reports
 //@access   Admin
-const getAllReports = async (req, res, next) => { 
-  try {
-    const report = await model.Report.find();
+const getAllReports = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.find();
 
-    res.status(200).json({
-      success: true,
-      count: report.length,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: report.length,
+    data: report
+  });
+});
 
 //@des      get one report
 //@route    GET /reports/:id
 //@access   Admin
-const getOneReport = async (req, res, next) => {
-  try {
-    const report = await model.Report.findById(req.params.id);
+const getOneReport = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.findById(req.params.id);
 
-    if (!report) {
-      return res.status(400).json({ success: false });
-    }
+  if (!report) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: report
+  });
+});
 
 //@des      Create reports
 //@route    POST /reports/new
 //@access   Admin
-const createReports = async (req, res, next) => {
-  try {
-    const report = await model.Report.create(req.body);
+const createReports = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(201).json({
+    success: true,
+    data: report
+  });
+});
 
 //@des      Update report
 //@route    PUT /reports/update/id
 //@access   Admin
-const updateReport = async (req, res, next) => {
-  try {
-    const report = await model.Report.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateReport = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!report) {
-      return res.status(400).json({ success: false });
-    }
+  if (!report) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: report
+  });
+});
 
 //@des      Delete report
 //@route    DELETE /reports/delete/id
 //@access   Admin
-const deleteReport = async (req, res, next) => {
-  try {
-    const report = await model.Report.findByIdAndDelete(req.params.id);
+const deleteReport = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.findByIdAndDelete(req.params.id);
 
-    if (!report) {
-      return res.status(400).json({ success: false });
-    }
+  if (!report) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }};
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllReports,
@@ -98,5 +84,4 @@ module.exports = {
   createReports,
   updateReport,
   deleteReport
- 
-}
+};
--- apps/student/report/report.handlers.js
+++ apps/student/report/report.handlers.js
@@ -1,44 +1,37 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all reports
 //@route    GET /reports
 //@access   Student
-const getAllReports = async (req, res, next) => { 
-  try {
-    const report = await model.Report.find();
+const getAllReports = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.find();
 
-    res.status(200).json({
-      success: true,
-      count: report.length,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: report.length,
+    data: report
+  });
+});
 
 //@des      get one report
 //@route    GET /reports/:id
 //@access   Student
-const getOneReport = async (req, res, next) => {
-  try {
-    const report = await model.Report.findById(req.params.id);
+const getOneReport = asyncHandler(async (req, res, next) => {
+  const report = await model.Report.findById(req.params.id);
 
-    if (!report) {
-      return res.status(400).json({ success: false });
-    }
+  if (!report) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: report
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
-
+  res.status(200).json({
+    success: true,
+    data: report
+  });
+});
 
 module.exports = {
   getAllReports,
-  getOneReport,
-
- 
-}
+  getOneReport
+};
