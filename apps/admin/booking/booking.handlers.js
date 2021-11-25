090b96c498faa780626fb3a06099d3439e64eddf
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 10:39:11 +0000

    Updated booking handler in student and admin route to use error response,  error middleware and async handler.

--- apps/admin/booking/booking.handlers.js
+++ apps/admin/booking/booking.handlers.js
@@ -1,100 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
-//@des      get all bookings 
+//@des      get all bookings
 //@route    GET /bookings
-//@access   Admin 
-const getAllBookings = async (req, res, next) => {
-  try {
-    const booking = await model.Booking.find();
+//@access   Admin
+const getAllBookings = asyncHandler(async (req, res, next) => {
+  const booking = await model.Booking.find();
 
-    res.status(200).json({
-      success: true,
-      count: booking.length,
-      data: booking
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: booking.length,
+    data: booking
+  });
+});
 
 //@des      get one booking
 //@route    GET /bookings/:id
 //@access   Admin
-const getOneBooking= async (req, res, next) => {
-  try {
-    const booking = await model.Booking.findById(req.params.id);
+const getOneBooking = asyncHandler(async (req, res, next) => {
+  const booking = await model.Booking.findById(req.params.id);
 
-    if (!booking) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: booking
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!booking) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: booking
+  });
+});
 
 //@des      Create bookings
 //@route    POST /bookings/new
 //@access   Admin
-const createBookings = async (req, res, next) => {
-  try {
-    const booking = await model.Booking.create(req.body);
+const createBookings = asyncHandler(async (req, res, next) => {
+  const booking = await model.Booking.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: booking
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: booking
+  });
+});
 
 //@des      Update booking
 //@route    PUT /bookings/update/id
 //@access   Admin
-const updateBooking = async (req, res, next) => {
-  try {
-    const booking = await model.Booking.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateBooking = asyncHandler(async (req, res, next) => {
+  const booking = await model.Booking.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!booking) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: booking
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!booking) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: booking
+  });
+});
 
 //@des      Delete book
 //@route    DELETE /Bookings/delete/id
 //@access   Admin
-const deleteBooking = async (req, res, next) => {
-  try {
-    const booking = await model.Booking.findByIdAndDelete(req.params.id);
+const deleteBooking = asyncHandler(async (req, res, next) => {
+  const booking = await model.Booking.findByIdAndDelete(req.params.id);
 
-    if (!booking) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!booking) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllBookings,
