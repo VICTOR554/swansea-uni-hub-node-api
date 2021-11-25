ebad81c97ee72faddf6cb87b2aaa5b6dd649a8b8
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 10:10:12 +0000

    updated student handler in admin route to use async handler instead of try and catch

--- apps/admin/student/student.handlers.js
+++ apps/admin/student/student.handlers.js
@@ -1,65 +1,57 @@
 const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all students
 //@route    GET /students
 //@access   Admin
-const getAllStudents = async (req, res, next) => {
-  try {
-    const student = await model.Student.find();
+const getAllStudents = asyncHandler(async (req, res, next) => {
+  const student = await model.Student.find();
 
-    res.status(200).json({
-      success: true,
-      count: student.length,
-      data: student
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: student.length,
+    data: student
+  });
+});
 
 //@des      get one student
 //@route    GET /students/:id
 //@access   Admin
-const getOneStudent = async (req, res, next) => {
-  try {
+const getOneStudent = asyncHandler(async (req, res, next) => {
+  
     const student = await model.Student.findById(req.params.id);
 
     if (!student) {
-      return     next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
-
+      return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
     }
 
     res.status(200).json({
       success: true,
       data: student
     });
-  } catch (err) {
-    next(err);
 
-  }
-};
+});
 
 //@des      Create students
 //@route    POST /students/new
 //@access   Admin
-const createStudents = async (req, res, next) => {
-  try {
+const createStudents = asyncHandler(async (req, res, next) => {
+ 
     const student = await model.Student.create(req.body);
 
     res.status(201).json({
       success: true,
       data: student
     });
-  } catch (err) {
-    next(err);  }
-};
+ 
+});
 
 //@des      Update student
 //@route    PUT /students/update/id
 //@access   Admin
-const updateStudent = async (req, res, next) => {
-  try {
+const updateStudent = asyncHandler(async (req, res, next) => {
+
     const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
       new: true,
       runValidators: true
@@ -73,16 +65,14 @@ const updateStudent = async (req, res, next) => {
       success: true,
       data: student
     });
-  } catch (err) {
-    next(err);
-    }
-};
+  
+});
 
 //@des      Delete student
 //@route    DELETE /students/delete/id
 //@access   Admin
-const deleteStudent = async (req, res, next) => {
-  try {
+const deleteStudent = asyncHandler(async (req, res, next) => {
+ 
     const student = await model.Student.findByIdAndDelete(req.params.id);
 
     if (!student) {
@@ -93,10 +83,8 @@ const deleteStudent = async (req, res, next) => {
       success: true,
       data: {}
     });
-  } catch (err) {
-    next(err);
-    }
-};
+  
+});
 
 module.exports = {
   getAllStudents,
