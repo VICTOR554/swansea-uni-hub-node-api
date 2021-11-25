080be7e544799c1ed2ef74edda35b419d159981e
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 11:32:41 +0000

    Updated term handler in admin route to use error response, error handler and async handler.

--- apps/admin/term/term.handlers.js
+++ apps/admin/term/term.handlers.js
@@ -1,96 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all terms
 //@route    GET /terms
 //@access   Admin
-const getAllTerms = async (req, res, next) => { 
-  try {
-    const term = await model.Term.find();
+const getAllTerms = asyncHandler(async (req, res, next) => {
+  const term = await model.Term.find();
 
-    res.status(200).json({
-      success: true,
-      count: term.length,
-      data: term
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    count: term.length,
+    data: term
+  });
+});
 
 //@des      get one term
 //@route    GET /terms/:id
 //@access   Admin
-const getOneTerm = async (req, res, next) => {
-  try {
-    const term = await model.Term.findById(req.params.id);
+const getOneTerm = asyncHandler(async (req, res, next) => {
+  const term = await model.Term.findById(req.params.id);
 
-    if (!term) {
-      return res.status(400).json({ success: false });
-    }
+  if (!term) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: term
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: term
+  });
+});
 
 //@des      Create terms
 //@route    POST /terms/new
 //@access   Admin
-const createTerms = async (req, res, next) => {
-  try {
-    const term = await model.Term.create(req.body);
+const createTerms = asyncHandler(async (req, res, next) => {
+  const term = await model.Term.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: term
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(201).json({
+    success: true,
+    data: term
+  });
+});
 
 //@des      Update term
 //@route    PUT /terms/update/id
 //@access   Admin
-const updateTerm = async (req, res, next) => {
-  try {
-    const term = await model.Term.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateTerm = asyncHandler(async (req, res, next) => {
+  const term = await model.Term.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!term) {
-      return res.status(400).json({ success: false });
-    }
+  if (!term) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: term
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }  };
+  res.status(200).json({
+    success: true,
+    data: term
+  });
+});
 
 //@des      Delete term
 //@route    DELETE /terms/delete/id
 //@access   Admin
-const deleteTerm = async (req, res, next) => {
-  try {
-    const term = await model.Term.findByIdAndDelete(req.params.id);
+const deleteTerm = asyncHandler(async (req, res, next) => {
+  const term = await model.Term.findByIdAndDelete(req.params.id);
 
-    if (!term) {
-      return res.status(400).json({ success: false });
-    }
+  if (!term) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }};
-
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllTerms,
@@ -98,5 +84,4 @@ module.exports = {
   createTerms,
   updateTerm,
   deleteTerm
- 
-}
+};
