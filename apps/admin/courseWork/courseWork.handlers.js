d3b5e1c7167b7310f9eb41523533a06e3b26f56b
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 10:52:15 +0000

    Updated coursework handler in student and admin route to use error response, error handler and async handler.

--- apps/admin/courseWork/courseWork.handlers.js
+++ apps/admin/courseWork/courseWork.handlers.js
@@ -1,100 +1,82 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all courseworks
 //@route    GET /courseworks
 //@access   Admin
-const getAllCourseworks= async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.find();
+const getAllCourseworks = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.find();
 
-    res.status(200).json({
-      success: true,
-      count: coursework.length,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: coursework.length,
+    data: coursework
+  });
+});
 
 //@des      get one coursework
 //@route    GET /courseworks/:id
 //@access   Admin
-const getOneCoursework = async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.findById(req.params.id);
+const getOneCoursework = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.findById(req.params.id);
 
-    if (!coursework) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!coursework) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: coursework
+  });
+});
 
 //@des      Create courseworks
 //@route    POST /courseworks/new
 //@access   Admin
-const createCourseworks = async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.create(req.body);
+const createCourseworks = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(201).json({
+    success: true,
+    data: coursework
+  });
+});
 
 //@des      Update coursework
 //@route    PUT /courseworks/update/id
 //@access   Admin
-const updateCoursework = async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateCoursework = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!coursework) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!coursework) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: coursework
+  });
+});
 
 //@des      Delete coursework
 //@route    DELETE /courseworks/delete/id
 //@access   Admin
-const deleteCoursework = async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.findByIdAndDelete(req.params.id);
+const deleteCoursework = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.findByIdAndDelete(req.params.id);
 
-    if (!coursework) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!coursework) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
 
 module.exports = {
   getAllCourseworks,
--- apps/student/coursework/coursework.handlers.js
+++ apps/student/coursework/coursework.handlers.js
@@ -1,44 +1,37 @@
+const ErrorResponse = require('../../../utils/errorResponse');
+const asyncHandler = require('../../../middleware/async');
 const model = require('../../../models/model');
 
 //@des      get all courseworks
 //@route    GET /courseworks
 //@access   Student
-const getAllCourseworks= async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.find();
+const getAllCourseworks = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.find();
 
-    res.status(200).json({
-      success: true,
-      count: coursework.length,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({
+    success: true,
+    count: coursework.length,
+    data: coursework
+  });
+});
 
 //@des      get one coursework
 //@route    GET /courseworks/:id
 //@access   Student
-const getOneCoursework = async (req, res, next) => {
-  try {
-    const coursework = await model.Coursework.findById(req.params.id);
+const getOneCoursework = asyncHandler(async (req, res, next) => {
+  const coursework = await model.Coursework.findById(req.params.id);
 
-    if (!coursework) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: coursework
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!coursework) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
 
+  res.status(200).json({
+    success: true,
+    data: coursework
+  });
+});
 
 module.exports = {
   getAllCourseworks,
-  getOneCoursework,
+  getOneCoursework
 };
