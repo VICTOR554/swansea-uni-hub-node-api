eea829d9bf5150e07ecc80dc823f086aeafe865d
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-11-25 10:23:10 +0000

    Updated student handler in student route to have error response,  error middleware and async handler.

--- apps/admin/student/student.handlers.js
+++ apps/admin/student/student.handlers.js
@@ -19,71 +19,63 @@ const getAllStudents = asyncHandler(async (req, res, next) => {
 //@route    GET /students/:id
 //@access   Admin
 const getOneStudent = asyncHandler(async (req, res, next) => {
-  
-    const student = await model.Student.findById(req.params.id);
+  const student = await model.Student.findById(req.params.id);
 
-    if (!student) {
-      return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
-    }
+  if (!student) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: student
-    });
-
+  res.status(200).json({
+    success: true,
+    data: student
+  });
 });
 
 //@des      Create students
 //@route    POST /students/new
 //@access   Admin
 const createStudents = asyncHandler(async (req, res, next) => {
- 
-    const student = await model.Student.create(req.body);
+  const student = await model.Student.create(req.body);
 
-    res.status(201).json({
-      success: true,
-      data: student
-    });
- 
+  res.status(201).json({
+    success: true,
+    data: student
+  });
 });
 
 //@des      Update student
 //@route    PUT /students/update/id
 //@access   Admin
 const updateStudent = asyncHandler(async (req, res, next) => {
+  const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+  if (!student) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    if (!student) {
-      return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
-    }
-
-    res.status(200).json({
-      success: true,
-      data: student
-    });
-  
+  res.status(200).json({
+    success: true,
+    data: student
+  });
 });
 
 //@des      Delete student
 //@route    DELETE /students/delete/id
 //@access   Admin
 const deleteStudent = asyncHandler(async (req, res, next) => {
- 
-    const student = await model.Student.findByIdAndDelete(req.params.id);
+  const student = await model.Student.findByIdAndDelete(req.params.id);
 
-    if (!student) {
-      return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
-    }
+  if (!student) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
+  }
 
-    res.status(200).json({
-      success: true,
-      data: {}
-    });
-  
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
 });
 
 module.exports = {
--- apps/student/student.handlers.js
+++ apps/student/student.handlers.js
@@ -1,47 +1,42 @@
+const ErrorResponse = require('../../utils/errorResponse');
+const asyncHandler = require('../../middleware/async');
 const model = require('../../models/model');
 
 //@des      get one student
 //@route    GET /students/:id
 //@access   Student
-const getOneStudent = async (req, res, next) => {
-  try {
-    const student = await model.Student.findById(req.params.id);
+const getOneStudent = asyncHandler(async (req, res, next) => {
+  const student = await model.Student.findById(req.params.id);
 
-    if (!student) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: student
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!student) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: student
+  });
+});
 
 //@des      Update student
 //@route    PUT /students/update/id
 //@access   Student
-const updateStudent = async (req, res, next) => {
-  try {
-    const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+const updateStudent = asyncHandler(async (req, res, next) => {
+  const student = await model.Student.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    if (!student) {
-      return res.status(400).json({ success: false });
-    }
-
-    res.status(200).json({
-      success: true,
-      data: student
-    });
-  } catch (err) {
-    res.status(400).json({ success: false });
+  if (!student) {
+    return next(new ErrorResponse(`Student is not in the database with the id of ${req.params.id}`, 404));
   }
-};
+
+  res.status(200).json({
+    success: true,
+    data: student
+  });
+});
+
 
 
 module.exports = {
