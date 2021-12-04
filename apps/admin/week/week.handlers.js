77031e4d77df694f118517213de91ed987a5b1d0
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-04 15:10:52 +0000

    remored create and update week handler, as it is not needed

--- apps/admin/week/week.handlers.js
+++ apps/admin/week/week.handlers.js
@@ -25,37 +25,6 @@ const getOneWeek = asyncHandler(async (req, res, next) => {
   });
 });
 
-//@des      Create weeks
-//@route    POST /weeks/new
-//@access   Admin
-const createWeeks = asyncHandler(async (req, res, next) => {
-  const week = await model.Week.create(req.body);
-
-  res.status(201).json({
-    success: true,
-    data: week
-  });
-});
-
-//@des      Update week
-//@route    PUT /weeks/update/id
-//@access   Admin
-const updateWeek = asyncHandler(async (req, res, next) => {
-  const week = await model.Week.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-
-  if (!week) {
-    return next(new ErrorResponse(`Week is not in the database with the id of ${req.params.id}`, 404));
-  }
-
-  res.status(200).json({
-    success: true,
-    data: week
-  });
-});
-
 //@des      Delete week
 //@route    DELETE /weeks/delete/id
 //@access   Admin
