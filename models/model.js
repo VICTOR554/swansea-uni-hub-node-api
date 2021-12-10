98a61d0ffebebeee00058b04d252b5f52ab83721
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-10 00:00:00 +0000

    test

--- models/model.js
+++ models/model.js
@@ -7,28 +7,28 @@ const user = mongoose.connect(process.env.MONGO_URI, {
     useUnifiedTopology: true
   });
 
-const Activity = mongoose.user('Activity', require('./activity-schemas/activity.schema'), 'activities');
-const Book = mongoose.user('Book', require('./book-schemas/book.schema'), 'books');
-const Booking = mongoose.user('Booking', require('./booking-schemas/booking.schema'), 'booking');
-const Building = mongoose.user('Building', require('./building-schemas/building.schema'), 'buildings');
-const Campus = mongoose.user('Campus', require('./campus-schemas/campus.schema'), 'campuses');
-const Course = mongoose.user('Course', require('./course-schemas/course.schema'), 'courses');
-const Coursework = mongoose.user('Coursework', require('./coursework-schemas/coursework.schema'), 'courseworks');
-const Event = mongoose.user('Event', require('./event-schemas/event.schema'), 'events');
-const Finance = mongoose.user('Finance', require('./finance-schemas/finance.schema'), 'finances');
-const Lecturer = mongoose.user('Lecturer', require('./lecturer-schemas/lecturer.schema'), 'lecturers');
-const Location = mongoose.user('Location', require('./location-schemas/location.schema'), 'locations');
-const Module =  mongoose.user('Module', require('./module-schemas/module.schema'), 'modules');
-const Note =  mongoose.user('Note', require('./note-schemas/note.schema'), 'notes');
-const Report = mongoose.user('Report', require('./report-schemas/report.schema'));
-const Room = mongoose.user('Room', require('./room-schemas/room.schema'), 'rooms');
-const Session = mongoose.user('Session', require('./session-schemas/session.schema'), 'sessions');
-const Society = mongoose.user('Society', require('./society-schemas/society.schema'), 'socities');
-const Sport = mongoose.user('Sport', require('./sport-schemas/sport.schema'), 'sports');
-const Student =  mongoose.user('Student', require('./student-schemas/student.schema'), 'students');
-const Task = mongoose.user('Task', require('./task-schemas/task.schema'), 'tasks');
-const Term = mongoose.user('Term', require('./term-schemas/term.schema'), 'terms');
-const Week = mongoose.user('Week', require('./week-schemas/week.schema'), 'weeks');
+const Activity = user.model('Activity', require('./activity-schemas/activity.schema'), 'activities');
+const Book = user.model('Book', require('./book-schemas/book.schema'), 'books');
+const Booking = user.model('Booking', require('./booking-schemas/booking.schema'), 'booking');
+const Building = user.model('Building', require('./building-schemas/building.schema'), 'buildings');
+const Campus = user.model('Campus', require('./campus-schemas/campus.schema'), 'campuses');
+const Course = user.model('Course', require('./course-schemas/course.schema'), 'courses');
+const Coursework = user.model('Coursework', require('./coursework-schemas/coursework.schema'), 'courseworks');
+const Event = user.model('Event', require('./event-schemas/event.schema'), 'events');
+const Finance = user.model('Finance', require('./finance-schemas/finance.schema'), 'finances');
+const Lecturer = user.model('Lecturer', require('./lecturer-schemas/lecturer.schema'), 'lecturers');
+const Location = user.model('Location', require('./location-schemas/location.schema'), 'locations');
+const Module =  user.model('Module', require('./module-schemas/module.schema'), 'modules');
+const Note =  user.model('Note', require('./note-schemas/note.schema'), 'notes');
+const Report = user.model('Report', require('./report-schemas/report.schema'));
+const Room = user.model('Room', require('./room-schemas/room.schema'), 'rooms');
+const Session = user.model('Session', require('./session-schemas/session.schema'), 'sessions');
+const Society = user.model('Society', require('./society-schemas/society.schema'), 'socities');
+const Sport = user.model('Sport', require('./sport-schemas/sport.schema'), 'sports');
+const Student =  user.model('Student', require('./student-schemas/student.schema'), 'students');
+const Task = user.model('Task', require('./task-schemas/task.schema'), 'tasks');
+const Term = user.model('Term', require('./term-schemas/term.schema'), 'terms');
+const Week = user.model('Week', require('./week-schemas/week.schema'), 'weeks');
 
 
 
