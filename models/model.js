9677ea3e110740db0b3583a203ec957bd6789baf
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2021-12-10 01:22:08 +0000

    test

--- models/model.js
+++ models/model.js
@@ -10,34 +10,36 @@ const mongoose = require('mongoose');
 //   useNewUrlParser: true,
 //   useUnifiedTopology: true
 // });
-const user = mongoose.createConnection(process.env.MONGO_URI.toString(),
+mongoose.connect(process.env.MONGO_URI.toString(),
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     });
 
-const Activity = user.model('Activity', require('./activity-schemas/activity.schema'), 'activities');
-const Book = user.model('Book', require('./book-schemas/book.schema'), 'books');
-const Booking = user.model('Booking', require('./booking-schemas/booking.schema'), 'bookings');
-const Building = user.model('Building', require('./building-schemas/building.schema'), 'buildings');
-const Campus = user.model('Campus', require('./campus-schemas/campus.schema'), 'campuses');
-const Course = user.model('Course', require('./course-schemas/course.schema'), 'courses');
-const Coursework = user.model('Coursework', require('./coursework-schemas/coursework.schema'), 'courseworks');
-const Event = user.model('Event', require('./event-schemas/event.schema'), 'events');
-const Finance = user.model('Finance', require('./finance-schemas/finance.schema'), 'finances');
-const Lecturer = user.model('Lecturer', require('./lecturer-schemas/lecturer.schema'), 'lecturers');
-const Location = user.model('Location', require('./location-schemas/location.schema'), 'locations');
-const Module = user.model('Module', require('./module-schemas/module.schema'), 'modules');
-const Note = user.model('Note', require('./note-schemas/note.schema'), 'notes');
-const Report = user.model('Report', require('./report-schemas/report.schema'), 'reports');
-const Room = user.model('Room', require('./room-schemas/room.schema'), 'rooms');
-const Session = user.model('Session', require('./session-schemas/session.schema'), 'sessions');
-const Society = user.model('Society', require('./society-schemas/society.schema'), 'socities');
-const Sport = user.model('Sport', require('./sport-schemas/sport.schema'), 'sports');
-const Student = user.model('Student', require('./student-schemas/student.schema'), 'students');
-const Task = user.model('Task', require('./task-schemas/task.schema'), 'tasks');
-const Term = user.model('Term', require('./term-schemas/term.schema'), 'terms');
-const Week = user.model('Week', require('./week-schemas/week.schema'), 'weeks');
+    var db = mongoose.connection;
+
+const Activity = db.model('Activity', require('./activity-schemas/activity.schema'), 'activities');
+const Book = db.model('Book', require('./book-schemas/book.schema'), 'books');
+const Booking = db.model('Booking', require('./booking-schemas/booking.schema'), 'bookings');
+const Building = db.model('Building', require('./building-schemas/building.schema'), 'buildings');
+const Campus = db.model('Campus', require('./campus-schemas/campus.schema'), 'campuses');
+const Course = db.model('Course', require('./course-schemas/course.schema'), 'courses');
+const Coursework = db.model('Coursework', require('./coursework-schemas/coursework.schema'), 'courseworks');
+const Event = db.model('Event', require('./event-schemas/event.schema'), 'events');
+const Finance = db.model('Finance', require('./finance-schemas/finance.schema'), 'finances');
+const Lecturer = db.model('Lecturer', require('./lecturer-schemas/lecturer.schema'), 'lecturers');
+const Location = db.model('Location', require('./location-schemas/location.schema'), 'locations');
+const Module = db.model('Module', require('./module-schemas/module.schema'), 'modules');
+const Note = db.model('Note', require('./note-schemas/note.schema'), 'notes');
+const Report = db.model('Report', require('./report-schemas/report.schema'), 'reports');
+const Room = db.model('Room', require('./room-schemas/room.schema'), 'rooms');
+const Session = db.model('Session', require('./session-schemas/session.schema'), 'sessions');
+const Society = db.model('Society', require('./society-schemas/society.schema'), 'socities');
+const Sport = db.model('Sport', require('./sport-schemas/sport.schema'), 'sports');
+const Student = db.model('Student', require('./student-schemas/student.schema'), 'students');
+const Task = db.model('Task', require('./task-schemas/task.schema'), 'tasks');
+const Term = db.model('Term', require('./term-schemas/term.schema'), 'terms');
+const Week = db.model('Week', require('./week-schemas/week.schema'), 'weeks');
 
 module.exports = {
   Activity,
